58b54addc3bc37218da8a290320b1d568cc4a40e
szef-2002 <szef-2002@go2.pl>
2024-06-05 23:54:19 +0200

    Comments added

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            MyFrame myFrame = new MyFrame();
-        });
+        SwingUtilities.invokeLater(MyFrame::new);
     }
 }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.net.URL;
 import java.util.Objects;
 
+//Class for Game Window, responsible for User Interface
 public class MyFrame extends JFrame {
 
     public static JLabel score;
@@ -11,50 +12,56 @@ public class MyFrame extends JFrame {
     public static JLabel logoLabel;
     public static JPanel topPanel;
     public static JPanel scorePanel;
-    private final ImageIcon logo;
 
     public MyFrame() throws HeadlessException {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Snake");
 
+        //Load and set window icon
+        URL iconURL = getClass().getResource("/images/icon.png");
+        Image icon = Toolkit.getDefaultToolkit().getImage(iconURL);
+        setIconImage(icon);
+
+        //Top Panel for everything
         topPanel = new JPanel();
         topPanel.setBackground(new Color(223, 197, 244));
         topPanel.setOpaque(true);
         topPanel.setLayout(new BorderLayout());
 
-        logo = new ImageIcon(Objects.requireNonNull(getClass().getResource("images/logo.png")));
+        //Load and set logo
+        ImageIcon logo = new ImageIcon(Objects.requireNonNull(getClass().getResource("images/logo.png")));
         logoLabel = new JLabel(logo, SwingConstants.LEFT);
 
+        //Initialize labels for current score
         score = new JLabel("Your Score: ", SwingConstants.RIGHT);
         score.setFont(new Font("Neue Helvetica", Font.BOLD, 20));
         score.setBackground(new Color(223, 197, 244));
         score.setOpaque(true);
 
+        //Initialize labels for best score
         bestScore = new JLabel("Best score is: ", SwingConstants.RIGHT);
         bestScore.setFont(new Font("Neue Helvetica", Font.BOLD, 20));
         bestScore.setBackground(new Color(223, 197, 244));
         bestScore.setOpaque(true);
 
+        //Panel for displaying scores
         scorePanel = new JPanel();
         scorePanel.setLayout(new BoxLayout(scorePanel, BoxLayout.Y_AXIS));
         scorePanel.setBackground(new Color(223, 197, 244));
         scorePanel.add(score);
         scorePanel.add(bestScore);
-
         scorePanel.setBorder(new EmptyBorder(0, 0, 0, 10));
 
         topPanel.add(logoLabel, BorderLayout.WEST);
         topPanel.add(scorePanel, BorderLayout.EAST);
         topPanel.setBorder(new EmptyBorder(0, 0, 5, 0));
 
-        URL iconURL = getClass().getResource("/images/icon.png");
-        Image icon = Toolkit.getDefaultToolkit().getImage(iconURL);
-        setIconImage(icon);
+        //Add Top Panel to the window
+        add(topPanel, BorderLayout.NORTH);
 
+        //Add Game to the window
         Game game = new Game();
         add(game);
-
-        add(topPanel, BorderLayout.NORTH);
 
         pack();
         setLocationRelativeTo(null);
